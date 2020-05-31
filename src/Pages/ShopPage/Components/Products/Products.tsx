2142631c45f12d1f40889d20e8ebd41f68f4e18e
Streeterxs <afonso_mg3@hotmail.com>
2020-05-31 14:26:53 -0300

    ShopPage - Base structure to build components.

---
+++ src/Pages/ShopPage/Components/Products/Products.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Product} from './';
+
+const Products = () => {
+
+    return (
+        <div>
+            products list
+            <Product/>
+        </div>
+    );
+};
+
+export default Products;
--- src/Pages/ShopPage/index.ts
+++ src/Pages/ShopPage/index.ts
@@ -1,1 +1,2 @@
+export * from './Components';
 export {default as ShopPage} from './ShopPage';
